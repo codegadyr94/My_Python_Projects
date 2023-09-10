7dd4af06cff715f712bd7085506d4008e769f050
Gadyr <139173058+codegadyr94@users.noreply.github.com>
2023-09-10 12:48:36 +0300

    Python Projects
    basic projects in python

---
+++ 2 add two digit number/ad two digit number.py
@@ -0,0 +1,5 @@
+two_digit_number = input("Type a two digit number: ")
+a = int(two_digit_number [0])
+b = int(two_digit_number [1])
+print(a + b)
+input("Press Enter to exit...")
---
+++ 3_BIM_test/version_0001/BMI_test.py
@@ -0,0 +1,7 @@
+height = input("enter your height in m: ")  
+weight = input("enter your weight in kg: ")
+h = float(height)
+w = int(weight)
+bmi = int(w / h ** 2)
+print(bmi)
+input("Press Enter to exit...")
